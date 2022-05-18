8ce26100c4ce1c984470d5a081969d8f3437d79f
beomso0 <bumsookim98@naver.com>
2022-05-19 01:40:44 +0900

    add setup.py & README

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="custom_SASRec", 
+  version="0.0.1",
+  author="Beomso0",
+  author_email="univ3352@gmail.com",
+  description="customized SASRec",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/beomso0/custom_SASRec",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+  install_requires=requirements,
+)
