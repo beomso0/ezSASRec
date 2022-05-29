774fff911660ebb00a1e5b3802e94c17d884959a
beomso0 <bumsookim98@naver.com>
2022-05-29 18:46:50 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ setuptools.setup(
   packages=setuptools.find_packages(),
   classifiers=[
     "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
   ],
   python_requires='>=3.7',
   install_requires=requirements,
