82597cb5f623176eec1a4850ac2ad01081cbffa2
beomso0 <bumsookim98@naver.com>
2022-05-30 21:48:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="custom_SASRec", 
-  version="0.3.8",
+  version="0.3.9",
   author="Beomso0",
   author_email="univ3352@gmail.com",
   description="customized SASRec",
