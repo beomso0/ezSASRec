ad78c76a92052bf661930386cc20de998db50c34
beomso0 <bumsookim98@naver.com>
2022-06-05 14:57:37 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
@@ -7,18 +7,20 @@ with open("README.md", "r") as fh:
   long_description = fh.read()
 
 setuptools.setup(
-  name="custom_SASRec", 
-  version="0.8.9",
-  author="Beomso0",
-  author_email="univ3352@gmail.com",
-  description="customized SASRec",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  url="https://github.com/beomso0/custom_SASRec",
-  packages=setuptools.find_packages(),
-  classifiers=[
-    "Programming Language :: Python :: 3",
-  ],
-  python_requires='>=3.7',
-  install_requires=requirements,
+    name="ezSASRec", 
+    version="0.8.9",
+    author="Beomso0",
+    license='MIT',
+    author_email="univ3352@gmail.com",
+    description="easy SASRec",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/beomso0/ezSASRec",
+    packages=setuptools.find_packages(),
+    keywords='sasrec, recommendation, sequential',
+    classifiers=[
+      "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.7',
+    install_requires=requirements,
 )
